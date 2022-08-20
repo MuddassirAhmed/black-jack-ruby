5ef6ff734962d340168415515d960e285658693e
MuddassirAhmed <muddassir.eng@hotmail.com>
2022-08-21 01:47:59 +0500

    updated file names

--- black_jack.rb
+++ black_jack.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-require_relative 'class_player'
-require_relative 'class_deck'
+require_relative 'player'
+require_relative 'deck'
 
 # Game class
 class BlackJack
--- deck.rb
+++ deck.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require_relative 'class_card'
+require_relative 'card'
 
 # Deck class
 class Deck
